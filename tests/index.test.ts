64780456cbecc8cfd9a24c6edec79b295a2c3550
Michael Upton <uptonm@wit.edu>
2019-01-22 13:53:40 -0500

    Added in tests for new routes

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,25 +1,48 @@
-import createServer from '../src/server'
+import createServer from '../src/server';
 
-const fastify = createServer()
+const fastify = createServer();
 
-afterAll(() => fastify.close(() => null))
+afterAll(() => fastify.close(() => null));
 
 test('should return hello world', async () => {
   try {
-    const response = await fastify.inject({ method: 'GET', url: '/' })
-    expect(response.statusCode).toBe(200)
-    expect(JSON.parse(response.payload)).toEqual({ hello: 'world' })
+    const response = await fastify.inject({ method: 'GET', url: '/' });
+    expect(response.statusCode).toBe(200);
+    expect(JSON.parse(response.payload)).toEqual({ hello: 'world' });
   } catch (error) {
-    expect(error).toBeFalsy()
+    expect(error).toBeFalsy();
   }
-})
+});
 
 test('should return twitter handle', async () => {
-  const handle = '@ArrowoodTech'
+  const handle = '@ArrowoodTech';
   try {
-    const response = await fastify.inject({ method: 'GET', url: `/twitter?handle=${handle}` })
-    expect(JSON.parse(response.payload)).toEqual({ twitterHandle: handle })
+    const response = await fastify.inject({
+      method: 'GET',
+      url: `/twitter?handle=${handle}`
+    });
+    expect(JSON.parse(response.payload)).toEqual({ twitterHandle: handle });
   } catch (error) {
-    expect(error).toBeFalsy()
+    expect(error).toBeFalsy();
   }
-})
+});
+
+test('Should return a users object', async () => {
+  try {
+    const response = await fastify.inject({ method: 'GET', url: '/api/users' });
+    expect(JSON.parse(response.payload)).toEqual({
+      users: [{ first: 'Mike', last: 'Upton' }]
+    });
+  } catch (error) {
+    expect(error).toBeFalsy();
+  }
+});
+
+test('Should return a users object', async () => {
+  try {
+    const response = await fastify.inject({ method: 'GET', url: '/api/users' });
+    expect(response.statusCode).toEqual(200);
+  } catch (error) {
+    expect(error).toBeFalsy();
+  }
+});
