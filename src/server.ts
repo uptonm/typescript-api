7d3ec9d5c93f21fe20f8c314bb721e0ac28cb22f
Michael Upton <uptonm@wit.edu>
2019-01-22 13:35:36 -0500

    Init + Updates

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import * as Fastify from 'fastify';
+
+export default function createServer(opts?: Fastify.ServerOptions) {
+  const fastify = Fastify(opts);
+
+  /* eslint-disable-next-line no-unused-vars */
+  fastify.get('/', async (request, reply) => {
+    return { hello: 'world' };
+  });
+
+  /* eslint-disable-next-line no-unused-vars */
+  fastify.get('/twitter', async (request, reply) => {
+    return { twitterHandle: request.query.handle };
+  });
+
+  fastify.get('/api/users', async (request, reply) => {
+    return { users: [{ first: 'Mike', last: 'Upton' }] };
+  });
+
+  fastify.get('/api/timestamp', async (request, reply) => {
+    return { date: new Date() };
+  });
+
+  return fastify;
+}
